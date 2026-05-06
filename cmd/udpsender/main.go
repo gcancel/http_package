b4c8193dc5f4b5b184910e6931ef6a785ed73ada
gcancel <gmartin9892@gmail.com>
2026-05-05 22:00:01 -0500

    added udpsender func for udp testing

--- cmd/udpsender/main.go
+++ cmd/udpsender/main.go
@@ -1,13 +1,38 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"log"
 	"net"
+	"os"
 )
 
 func main() {
-	udp, err := net.ResolveUDPAddr("udp", ":")
+	udp, err := net.ResolveUDPAddr("udp", ":42069")
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	udpConnection, err := net.DialUDP("udp", nil, udp)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer udpConnection.Close()
+
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Printf("> ")
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		_, err = udpConnection.Write([]byte(input))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+	}
+
 }
