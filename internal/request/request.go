d71b98223d9438988bb1b1c73e39441208203675
gcancel <gmartin9892@gmail.com>
2026-05-12 21:37:28 -0500

    making changes to the request.go file to accept chunk data

--- internal/request/request.go
+++ internal/request/request.go
@@ -1,9 +1,16 @@
 package request
 
-import "io"
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"io"
+	"strings"
+)
 
 type Request struct {
-	RequestLine RequestLine
+	RequestLine  RequestLine
+	RequestState RequestState
 }
 
 type RequestLine struct {
@@ -12,7 +19,103 @@ type RequestLine struct {
 	Method        string
 }
 
+type RequestState int
+
+const (
+	Request_Initialized RequestState = iota
+	Request_Done
+)
+const bufferSize = 8
+
 func RequestFromReader(reader io.Reader) (*Request, error) {
 
-	return &Request{}, nil
+	buffer := make([]byte, bufferSize)
+	request := &Request{
+		RequestState: Request_Initialized,
+	}
+	readToIndex := 0
+
+	for request.RequestState != Request_Initialized {
+
+		if len(buffer) == cap(buffer) {
+			newBuffer := make([]byte, bufferSize*2)
+			copy(newBuffer, buffer)
+			buffer = newBuffer
+		}
+
+		r, err := reader.Read(buffer[:readToIndex])
+		if err != nil {
+			return nil, fmt.Errorf("error reading from buffer")
+		}
+		if errors.Is(err, io.EOF) {
+			request.RequestState = Request_Done
+		}
+		readToIndex = r
+
+		bytesRead, err := request.parse(buffer[:readToIndex])
+		if err != nil {
+			return nil, fmt.Errorf("error parsing bytes: %s Total bytes read: %d", err, bytesRead)
+		}
+
+		copy(buffer, make([]byte, 8))
+		readToIndex -= bytesRead
+
+	}
+
+	return request, nil
+}
+
+func parseRequestLine(lines []byte) (int, *RequestLine, error) {
+
+	idx := bytes.Index(lines, []byte("\r\n"))
+	if idx == -1 {
+		return 0, nil, nil
+	}
+
+	req := string(lines[:idx])
+	r := strings.Split(req, " ")
+	if len(r) < 3 {
+		fmt.Println(r)
+		return 0, nil, fmt.Errorf("Invalid HTTP request-line: %s", r)
+	}
+
+	method := r[0]
+	for _, c := range method {
+		if c < 'A' || c > 'Z' {
+			return 0, nil, fmt.Errorf("error: Invalid method in request-line")
+		}
+	}
+
+	target := r[1]
+	version := strings.TrimPrefix(r[2], "HTTP/")
+
+	return len(req), &RequestLine{HttpVersion: version, RequestTarget: target, Method: method}, nil
+}
+
+func (r *Request) parse(data []byte) (int, error) {
+	// TODO: implement this func
+	if r.RequestState == Request_Done {
+		return 0, fmt.Errorf("error trying to read data in a done state.")
+	}
+
+	if r.RequestState != Request_Initialized {
+		if r.RequestState != Request_Done {
+			return 0, fmt.Errorf("error: unknown RequestState.")
+		}
+	}
+
+	for r.RequestState == Request_Initialized {
+		pos, req, err := parseRequestLine(data)
+		if err != nil {
+			return 0, fmt.Errorf("Error parsing request-line: %s %d", err, pos)
+		}
+
+		if pos == 0 {
+			return 0, nil
+		}
+		r.RequestState = Request_Done
+		r.RequestLine = *req
+	}
+
+	return 0, nil
 }
