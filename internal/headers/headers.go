f1892a13afefdac1c9aca8a71b1ea5312e578664
gcancel <gmartin9892@gmail.com>
2026-06-06 00:50:24 -0500

    updated headers func to check for invalid characters in field name.

--- internal/headers/headers.go
+++ internal/headers/headers.go
@@ -34,6 +34,15 @@ func (h Headers) Parse(data []byte) (n int, done bool, err error) {
 		return 0, false, fmt.Errorf("error: Invalid header line %s", key)
 	}
 	key = strings.TrimSpace(key)
+
+	// checking for invalid characters in the field name
+	for _, c := range key {
+		if !isAlphaNumeric(c) {
+			return 0, false, fmt.Errorf("error: Invalid field name characters. %v | %s", c, key)
+		}
+	}
+
+	key = strings.ToLower(key)
 	value := bytes.TrimSpace(headerParts[1])
 
 	h.Set(key, string(value))
@@ -43,4 +52,11 @@ func (h Headers) Parse(data []byte) (n int, done bool, err error) {
 
 func (h Headers) Set(key, value string) {
 	h[key] = value
+}
+
+func isAlphaNumeric(c rune) bool {
+	if (c >= 'A' && c <= 'z') || (c >= '0' && c <= '9') {
+		return true
+	}
+	return false
 }
--- internal/headers/headers_test.go
+++ internal/headers/headers_test.go
@@ -14,13 +14,21 @@ func TestHeadersParse(t *testing.T) {
 	n, done, err := headers.Parse(data)
 	require.NoError(t, err)
 	require.NotNil(t, headers)
-	assert.Equal(t, "localhost:42069", headers["Host"])
+	assert.Equal(t, "localhost:42069", headers["host"])
 	assert.Equal(t, 23, n)
 	assert.False(t, done)
 
 	// Test: Invalid spacing header
 	headers = NewHeaders()
 	data = []byte("       Host: localhost:42069\r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.Error(t, err)
+	assert.Equal(t, 0, n)
+	assert.False(t, done)
+
+	// Test: Invalid header characters
+	headers = NewHeaders()
+	data = []byte("H©st: localhost:42069\r\n\r\n")
 	n, done, err = headers.Parse(data)
 	require.Error(t, err)
 	assert.Equal(t, 0, n)
